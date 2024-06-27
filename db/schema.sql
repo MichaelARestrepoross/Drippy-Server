e761c0f3d5e4da0e52f0ec19c37b3262b7209544
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-06-27 15:17:54 -0400

    Updated server with database and data to seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,45 @@
 -- db/schema.sql
-DROP DATABASE IF EXISTS debug_arena;
-CREATE DATABASE debug_arena;
+DROP DATABASE IF EXISTS drippy_db;
+CREATE DATABASE drippy_db;
 
-\c debug_arena;
+\c drippy_db;
 
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY ,
-    uid VARCHAR(255),
-    email VARCHAR(100),
-    username VARCHAR(100),
+    uid VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    username VARCHAR(100) UNIQUE,
     photo VARCHAR(100),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+-- CREATE TABLE wardrobe (
+--     id SERIAL PRIMARY KEY,
+--     user_id INT NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+--     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
+
+-- CREATE TABLE clothes (
+--     id SERIAL PRIMARY KEY,
+--     wardrobe_id INT NOT NULL,
+--     color VARCHAR(50) NOT NULL,
+--     type VARCHAR(50) NOT NULL,
+--     sub_type VARCHAR(50) NOT NULL,
+--     weather_type_id INT NOT NULL,
+--     material VARCHAR(50),
+--     prompt TEXT,
+--     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+--     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (wardrobe_id) REFERENCES wardrobe(id),
+--     FOREIGN KEY (weather_type_id) REFERENCES weather(id)
+-- );
+
+-- CREATE TABLE weather (
+--     id SERIAL PRIMARY KEY,
+--     weather_type VARCHAR(50) NOT NULL
+-- );
--- db/seed.sql
+++ db/seed.sql
@@ -5,3 +5,10 @@
 -- when seeding other fields in other tables and you need the foreign key of user Id, retrieve the userIds from looking on postico or using psql
 -- SELECT * from users 
 -- manually place the id into your INSERT INTO values
+
+-- Insert initial data into the users table
+INSERT INTO users (uid, email, username, photo, created_at, updated_at)
+VALUES 
+    ('SH4DtzZfODOmpwAIqwEUOXqHdWt2', 'firstemail@gmail.com', 'first_user', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('SH4DtzZfODOmpwAIqwEUOXqHdWt2', 'fakeemail@gmail.com', 'second_user', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('IeERyMSGueavSqJioTrRpCYuMBo1', 'thirdemail@gmai.com', 'third_user', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
